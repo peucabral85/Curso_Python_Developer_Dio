1fc9b9be19a72e3a354478c1bbcced8399cbe80f
Pericles Cabral <94022011+peucabral85@users.noreply.github.com>
2023-05-01 00:56:44 -0300

    Delete Desafios Básicos directory

--- Desafios Básicos/Desafio Encaixa ou Não.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-n = N
-while(n > 0):
-    a,b = input().split(" ")
-    if(b == a[len(a) - len(b):]):
-        print("encaixa")
-    else:
-        print ("não encaixa")    
-    n -= 1        
